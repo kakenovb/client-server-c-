59bc89bcb3dc53c04a179b05797d1f7b1a2bae5f
Baurzhan Kakenov <38176653+kakenovb@users.noreply.github.com>
2022-11-22 13:35:00 +0300

    Update wrappers.h

--- wrappers.h
+++ wrappers.h
@@ -4,16 +4,16 @@
 
 int Socket(int domain, int type, int protocol); 
 
-void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+void Bind(int sockfd, struct sockaddr* addr, socklen_t addrlen);
 
 void Listen(int sockfd, int backlog); 
 
-int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen); 
+int Accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen); 
 
-int Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen); 
+int Connect(int sockfd, struct sockaddr* addr, socklen_t addrlen); 
 
-void Inet_pton(int af, const char *src, void *dst); 
+void Inet_pton(int af, const char* src, void *dst); 
 
-int Read(int fd, void *buf, size_t count);
+int Read(int fd, void* buf, size_t count);
 
-void Write(int fd, const void *buf, size_t count);
+void Write(int fd, const void* buf, size_t count);
